64413fde8d117794599f86b1da98108a2ed73f07
nastya volkova <derric41@mail.ru>
2025-06-11 09:39:07 +0400

    2. Add i18next and locales

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
-// import * as yup from 'yup'
-// import { proxy, subscribe, snapshot } from 'valtio'
+import * as yup from 'yup'
+import i18n from 'i18next'
+import { proxy, subscribe, snapshot } from 'valtio/vanilla'
+import ru from './locales/ru.js'
 
-// const isValid = (url, feeds) => {
-//   const schema = yup
-//     .string()
-//     .url()
-//     .matches(/^(http|https):\/\/.*\.(xml|rss|atom)$/, 'Должен быть корректный RSS-URL')
-//     .notOneOf(feeds)
-//     .required()
-//   return schema.validateSync(url)
-// }
+const isValid = (url, feeds) => {
+  const schema = yup
+    .string()
+    .url()
+    .matches(/^(http|https).*(xml|rss|atom).*/)
+    .notOneOf(feeds)
+    .required()
+  return schema.validateSync(url)
+}
 
 // export default () => {
 //   const state = proxy({
@@ -35,6 +37,44 @@
 //   })
 // }
 
+const elements = {
+  form: document.querySelector('.rss-form'),
+  input: document.getElementById('url-input'),
+  submit: document.querySelector('[type="submit"]'),
+  feedbackMessage: document.querySelector('.feedback'),
+}
+
 export default () => {
-  console.log("index.js executed!")
+  const i18nInstance = i18n.createInstance()
+  i18nInstance.init({
+    lng: 'ru',
+    debug: false,
+    resources: {
+      ru,
+    },
+  })
+    .then(() => {
+      const title = document.querySelector('h1')
+      title.textContent = i18nInstance.t('project.title')
+    })
+
+  const state = proxy({
+    rssForm: {
+      status: 'filling',
+      error: [],
+      valid: false,
+    },
+    feeds: [],
+  })
+  elements.input.addEventListener('input', (e) => {
+    const url = e.target.value
+    if (isValid(url, state.feeds)) {
+      state.rssForm.valid = 'true'
+      state.feeds.push(url)
+    }
+    else {
+      state.rssForm.valid = 'false'
+    }
+    console.log(state.feeds)
+  })
 }
---
+++ src/locales/ru.js
@@ -0,0 +1,16 @@
+export default {
+  translation: {
+    project: {
+      title: 'RSS агрегатор',
+    },
+    form: {
+      success: 'RSS успешно загружен',
+      errors: {
+        notUniqueUrl: 'RSS уже существует',
+        notValidUrl: 'Ссылка должна быть валидным URL',
+        notValidRss: 'Ресурс не содержит валидный RSS',
+        networkProblems: 'Ошибка сети',
+      },
+    },
+  },
+}
